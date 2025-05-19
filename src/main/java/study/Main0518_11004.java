e1454cbff4d55efa3e250704b1345e668a9160f9
hanjihoon03 <gkswlgns7653@gmail.com>
2025-05-19 10:22:10 +0900

    알고리즘 스터디 0519 병합정렬 2751

---
+++ src/main/java/study/Main0518_11004.java
@@ -0,0 +1,32 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main0518_11004 {
+
+    //퀵 정렬 사용해보기
+    //퀵 정렬 -> 기준값을 선정해 해당 값 보다 작은 값과 큰 값으로 분류해 반복해 정렬하는 알고리즘 시간 복잡도(n log n)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[k - 1]);
+    }
+}
